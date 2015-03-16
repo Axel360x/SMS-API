91a8e6c0e19a8037e2d9252520e2faf27f00412e
MARVIN1994PL <marvin1994pl@gmail.com>
2015-03-16 13:50:13 +0100

    Update index.php

--- API/index.php
+++ API/index.php
@@ -1,31 +1,34 @@
 <?php 
     
-   //Przykladowe dane wyslane przez api 
-   $id_pay = '123';
-   $code = 'afwasdcwas';
-   $id_user = 'afwasdcwas';
-   $coment = urlencode('test');
-   $buyer = urlencode('test');
+    //Przykladowe dane wyslane przez api 
+    $id_pay = '1001';
+    //$id_pay = '1002';
+    //$id_pay = '3001';
+    //$id_pay = '4001';
+    $code = 'srvawdsy';
+    $id_user = '0001';
+    $coment = urlencode('test');
+    $buyer = urlencode('test');
 
     
-   $hendle = file_get_contents("http://s2.soruby.pl/api/api.php?idsms=".$id_pay."&code=".$code."&iduser=".$id_user."&coment=".$coment."&buyer=".$buyer); 
+    $hendle = file_get_contents("http://s2.soruby.pl/api/api.php?idsms=".$id_pay."&code=".$code."&iduser=".$id_user."&coment=".$coment."&buyer=".$buyer); 
    
-   ///api.php?idsms=123&code=afwasdcwas&iduser=afwasdcwas&coment=test&buyer=test
+    //api.php?idsms=1001&code=asrvawdsy&iduser=0001&coment=test&buyer=test
    
-   echo $hendle;
+    echo $hendle;
     
-   if($handle){ 
-       if($zhandle == '1'){ 
+    if($handle){ 
+        if($zhandle == '1'){ 
            echo 'Kod poprawny'; 
-       }elseif($handle == '2'){ 
+        }elseif($handle == '2'){ 
            echo 'Bledny klucz api'; 
-       }elseif($handle== '0'){ 
+        }elseif($handle== '0'){ 
            echo 'Bledny kod sms'; 
-       }else{ 
+        }else{ 
            echo 'Nieznany blad'; 
-       }    
-   }else{ 
+        }    
+    }else{ 
        echo 'Blad polaczenia z peratorem'; 
-   } 
+    } 
     
 ?>
