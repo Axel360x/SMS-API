218a0b5df7c7d59eac780ccb09b842e91236259c
Marcin Michalak <marvin1994pl@gmail.com>
2015-03-18 00:51:56 +0100

    zaczac od 49 linijki

--- API/api.php
+++ API/api.php
@@ -19,32 +19,34 @@
 			list($wallet1) = $db->tablica($rezultat); // tworze smienna $wallet1
 			unset($rezultat); // czyszczenie tablicy
 				}
-				
+	if(isset($_GET['code'])){
+		if(!preg_match("/^[A-Za-z0-9]{8}$/",$_GET['code'])) error(10); // bledny kod z sms			
 	if(isset($_GET['idsms'])){ //done?
 			$rezultat = $db->pytanie("SELECT * FROM sms_pay WHERE id_pay = {$_GET['idsms']}");
 			if(mysql_num_rows($rezultat) != 1){
 				error(7); //brak takiego id_pay
 			}else{
-			list(, $sufix, $numer, $cost, $id_acc, $inter) = $db->tablica($rezultat);
+			list(, $sufix, $numer, $cost, $acc_id, $inter) = $db->tablica($rezultat);
 			unset($rezultat); // czyszczenie tablicy
 				// 0 - HomePay 1 - CashBill
 				switch ($inter) {
     				case 0:
       				 unset($numer, $sufix);
-						if(isset($_GET['code'])){
 						 /*KOD MA WYKONYWAC PO DOSTARCZENIU CODE*/
-												}
+						 	$handle=fopen("http://homepay.pl/API/check_code.php?usr_id=".$config_homepay_usr_id."&acc_id=".$acc_id."&code=".$code,'r');
+							$check=fgets($handle,8);
+							fclose($handle);
+												
 						
      	   			 break;
 					case 1:
       				 unset($id_acc); 
-						if(isset($_GET['code'])){
-						 /*KOD MA WYKONYWAC PO DOSTARCZENIU CODE*/
-												}
-						
+						 /*KOD MA WYKONYWAC PO DOSTARCZENIU CODE*/						
      			 	 break;
 								}
 				}			}
+							 }else{error(9);} //brak code
+					//$check //echo
 														   }
 	
 	
--- API/config.php
+++ API/config.php
@@ -4,4 +4,6 @@ $host = 'localhost';
 $uzytkownik = 'root';
 $haslo = '';
 $bazadanych = 'sms';
+//dane do sms
+$config_homepay_usr_id=0;
 ?>
