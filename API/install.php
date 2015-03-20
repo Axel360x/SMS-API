5ec4c5d98f3c6fe8e3c42787d62b2915ec6bef78
Marcin Michalak <marvin1994pl@gmail.com>
2015-03-20 15:38:48 +0100

    install END

--- API/install.php
+++ API/install.php
@@ -46,7 +46,7 @@ catch(PDOException $e)
 	
 try {
     // sql to create table
-    $sql = "CREATE TABLE IF NOT EXISTS us (
+    $sql = "CREATE TABLE IF NOT EXISTS konta_us (
     id INT(3) UNSIGNED PRIMARY KEY, 
     secondname VARCHAR(30) NOT NULL,
 	country VARCHAR(10) NOT NULL,
@@ -60,7 +60,7 @@ try {
 
     // use exec() because no results are returned
     $conn->exec($sql);
-    echo "Table us created successfully<br>";
+    echo "Table konta_us created successfully<br>";
     }
 catch(PDOException $e)
     {
@@ -70,10 +70,10 @@ catch(PDOException $e)
 try {
     // sql to create table
     $sql = "CREATE TABLE IF NOT EXISTS sms_pay (
-    id INT(3) UNSIGNED PRIMARY KEY, 
-    id_acc INT(11) NOT NULL,
+    id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_acc INT(11),
 	sufix VARCHAR(10) NOT NULL,
-	numer INT(7) UNSIGNED NOT NULL,
+	numer INT(5) UNSIGNED NOT NULL,
 	cost INT(3) UNSIGNED NOT NULL,
 	inter INT(1) UNSIGNED NOT NULL,
     reg_date TIMESTAMP
@@ -107,6 +107,45 @@ catch(PDOException $e)
     {
     echo $sql . "<br>" . $e->getMessage();
     }
+
+try {
+    // sql to create table
+    $sql = "INSERT INTO sms_pay (id_acc, sufix, numer, cost, inter) VALUES
+	(20397, 'TAB', 7155, 1, 0),
+	(20398, 'TAB', 7955, 9, 0),
+	(20399, 'TAB', 92555, 25, 0),
+	(20400, 'TAB', 91955, 19, 0),
+	(20401, 'TAB', 7655, 6, 0),
+	(20402, 'TAB', 7455, 4, 0);";
+	//HOMEPAY
+    // use exec() because no results are returned
+    $conn->exec($sql);
+	
+    echo "Table sms_pay part 1 (first) insert successfully<br>";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+try {
+    // sql to create table
+    $sql = "INSERT INTO sms_pay (sufix, numer, cost, inter) VALUES
+	('MINE', 72480, 2, '1'),
+	('MINE', 73480, 3, '1');";
+	//http://goo.gl/4iFUEd //pisz na marvin1994pl@gmail.com aby uzyskac dostep
+	//CASHBILL
+    // use exec() because no results are returned
+    $conn->exec($sql);
+	
+    echo "Table sms_pay part 2 (last) insert successfully<br>";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+	
+	
 
 $conn = null;
 ?>
